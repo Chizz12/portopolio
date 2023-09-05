3cc1dddcce8d1c99d7c8bb9cd6ab65649c392566
Chizz12 <raketrtdf@gmail.com>
2023-09-05 19:43:10 +0700

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,15 @@
+AOS.init();
+(function() {
+  "use strict";
+
+    var typed = new Typed(".typed",{
+    strings: [,  "Programmer", "Freelancer"] ,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+
+    })
+
+
+})()
